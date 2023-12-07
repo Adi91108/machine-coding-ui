53c2d7c42420befbd05a1ef5ea1a08c74cea007c
“Adi91108” <adityasen911@gmail.com>
2023-12-07 16:20:13 +0530

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css'
-import InfiniteScroll from './components/InfiniteScroll/InfiniteScroll'
-import SearchBarPage from './components/SearchBar with Pagination/SeacrhBarPage'
+import Sidebar from './components/SideBar/Sidebar'
+// import InfiniteScroll from './components/InfiniteScroll/InfiniteScroll'
+// import SearchBarPage from './components/SearchBar with Pagination/SeacrhBarPage'
 import { TicTacToe } from './components/TicTacToe/TicTacToe'
-import Carousel from './components/carousel/Carousel'
+// import Carousel from './components/carousel/Carousel'
 // import Accordian from './components/Accordian/Accordian'
 // import AutoComplete from './components/AutoCompleteSearch/AutoComplete'
 // import Index from './components/Modal'
@@ -21,8 +22,9 @@ function App() {
      {/* <Accordian/> */}
      {/* <Carousel/> */}
      {/* <InfiniteScroll/> */}
-     <TicTacToe/>
-     <SearchBarPage/>
+     {/* <TicTacToe/> */}
+     {/* <SearchBarPage/> */}
+     <Sidebar/>
 
     </>
   )
---
+++ src/components/SideBar/Sidebar.jsx
@@ -0,0 +1,32 @@
+// Sidebar.js
+import { useState } from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+    
+
+   {<div className={`sidebar ${isOpen ? 'open' : ''}`}>
+   <button className="hamburger" onClick={toggleSidebar}>
+        ☰ 
+      </button>
+      <div className="menu">
+        {/* Add your menu items here */}
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+
+    </div>}
+    </>
+  );
+};
+
+export default Sidebar;
